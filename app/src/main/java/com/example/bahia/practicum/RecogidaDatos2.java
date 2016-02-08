d52a29be518fe165f05f9dc7a7499e3f57727b81
Ivan <ivan.bruna.garcia@gmail.com>
2016-02-08 13:37:44 +0100

    Añadida la 3ª vista de la recogida de datos

--- app/src/main/java/com/example/bahia/practicum/RecogidaDatos2.java
+++ app/src/main/java/com/example/bahia/practicum/RecogidaDatos2.java
@@ -1,12 +1,26 @@
 package com.example.bahia.practicum;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 
 public class RecogidaDatos2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recogida_datos2);
+        siguienteLayout();
+    }
+
+    private void siguienteLayout() {
+        Button siguiente = (Button) findViewById(R.id.siguiente_btn2);
+        siguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RecogidaDatos2.this, RecogidaDatos3.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/bahia/practicum/RecogidaDatos3.java
@@ -0,0 +1,13 @@
+package com.example.bahia.practicum;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class RecogidaDatos3 extends AppCompatActivity {
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_recogida_datos3);
+        }
+    }
+
