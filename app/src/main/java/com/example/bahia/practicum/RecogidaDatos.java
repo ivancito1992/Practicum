c1fb21808da6fa35211e9cd32eec179db27cf2ea
Ivan <ivan.bruna.garcia@gmail.com>
2016-02-08 12:41:15 +0100

    Modificado para acceder a la siguiente activity

--- app/src/main/java/com/example/bahia/practicum/RecogidaDatos.java
+++ app/src/main/java/com/example/bahia/practicum/RecogidaDatos.java
@@ -1,7 +1,10 @@
 package com.example.bahia.practicum;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RecogidaDatos extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class RecogidaDatos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recogida_datos);
+        siguienteLayout();
+    }
+
+    private void siguienteLayout() {
+        Button siguiente = (Button) findViewById(R.id.siguiente_btn);
+        siguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RecogidaDatos.this, RecogidaDatos2.class));
+            }
+        });
     }
 }
