ed0042ee72460a6336150b71898e317b496daa45
Ivan <ivan.bruna.garcia@gmail.com>
2016-02-08 12:42:47 +0100

    Modificado para acceder a la siguiente activity segunda parte

---
+++ app/src/main/java/com/example/bahia/practicum/RecogidaDatos2.java
@@ -0,0 +1,12 @@
+package com.example.bahia.practicum;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class RecogidaDatos2 extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recogida_datos2);
+    }
+}
