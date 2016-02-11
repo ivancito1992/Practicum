22e1281ec6c5beea09d6488d3951a35d4e38fe28
Iván Bruna García <ivan.bruna.garcia@gmail.com>
2016-02-11 14:14:31 +0100

    Fondo probado y base de datos creada vacia

---
+++ app/src/main/java/com/example/bahia/practicum/DataBaseManager.java
@@ -0,0 +1,20 @@
+package com.example.bahia.practicum;
+
+/**
+ * Created by ivanb on 11/02/2016.
+ */
+public class DataBaseManager{
+    public static final String TABLE_NAME = "datos";
+    public static final String CN_ID = "_id";
+    public static final String CN_PREGUNTAS = "Preguntas";
+    public static final String CN_MUCHO = "Mucho";
+    public static final String CN_POCO = "Poco";
+    public static final String CN_NADA = "Nada";
+
+    public static final String CREATE_TABLE =  "create table " +TABLE_NAME+ " ("
+            + CN_ID + " integer primary key autoincrement,"
+            + CN_PREGUNTAS + " text not null,"
+            + CN_MUCHO + " integer,"
+            + CN_POCO + " integer,"
+            + CN_NADA + " integer) ;";
+}
--- app/src/main/java/com/example/bahia/practicum/RecogidaDatosIniciar.java
+++ app/src/main/java/com/example/bahia/practicum/RecogidaDatosIniciar.java
@@ -1,6 +1,7 @@
 package com.example.bahia.practicum;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -8,9 +9,16 @@ import android.widget.Button;
 
 
 public class RecogidaDatosIniciar extends AppCompatActivity {
+
+    private boolean creadaBD = false;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iniciar_recogida_datos);
+
+        DataBaseHelper helper = new DataBaseHelper(this);
+        SQLiteDatabase db = helper.getWritableDatabase();
+
         siguienteLayout();
     }
 
